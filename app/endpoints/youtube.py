02f86b1cb44cc152ad177d0efe9269b0ae7bb6d2
GG.AI Labs <gui.agro@gmail.com>
2025-05-07 19:33:04 -0400

    Atualização Proxy

--- app/endpoints/youtube.py
+++ app/endpoints/youtube.py
@@ -1,19 +1,18 @@
+import os
 from fastapi import APIRouter, Query
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 
 router = APIRouter()
 
+PROXY_URL = os.getenv("PROXY_URL")  # pega do ambiente
+proxies = {"http": PROXY_URL, "https": PROXY_URL} if PROXY_URL else None
 
 @router.get("/transcript")
 def get_transcript(video_id: str = Query(..., description="ID do vídeo YouTube"),
                    lang: str = Query("pt,en,es", description="Idiomas preferidos separados por vírgula")):
-    """
-    Retorna a transcrição de um vídeo do YouTube, se disponível.
-    """
     try:
         langs = [l.strip() for l in lang.split(",")]
-        transcript = YouTubeTranscriptApi.get_transcript(
-            video_id, languages=langs)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=langs, proxies=proxies)
         return {
             "video_id": video_id,
             "languages": langs,
@@ -25,3 +24,4 @@ def get_transcript(video_id: str = Query(..., description="ID do vídeo YouTube"
         return {"error": "Nenhuma transcrição encontrada para os idiomas fornecidos."}
     except Exception as e:
         return {"error": str(e)}
+
